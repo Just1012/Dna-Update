249104166d487202dae732cd2c6f85ab1416205e
Mohamed Adel <99692050+Just1012@users.noreply.github.com>
2024-07-18 13:16:10 +0300

    .

--- app/Http/Controllers/Dashboard/PrivacyController.php
+++ app/Http/Controllers/Dashboard/PrivacyController.php
@@ -3,9 +3,52 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Privacy;
 use Illuminate\Http\Request;
+
+use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Support\Facades\Validator;
 
 class PrivacyController extends Controller
 {
-    //
+    public function editBigSponsor()
+    {
+        $privacy = Privacy::first();
+        return view('Dashboard.GeneralSetting.privacy', compact('privacy'));
+    }
+
+    // Method to update the existing row
+
+    public function updateBigSponsor(Request $request)
+    {
+        // Define validation rules
+        $rules = [
+            'privacy_ar' => 'nullable|longtext',
+            'privacy_en' => 'nullable|longtext',
+        ];
+
+        // Validate the request data
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        try {
+            // Update or create the sponsor page data
+            $privacy = Privacy::first() ?? new Privacy();
+
+            $privacy->privacy_ar = $request->privacy_ar;
+            $privacy->privacy_en = $request->privacy_en;
+
+            // Save the privacy page
+            $privacy->save();
+
+            Toastr::success(__('Privacy Section Updated Successfully'), __('Success'));
+            return redirect()->back();
+        } catch (\Exception $e) {
+            Toastr::error(__('An error occurred. Try Again'), __('Error'));
+            return redirect()->back()->withErrors(['error' => $e->getMessage()])->withInput();
+        }
+    }
 }
